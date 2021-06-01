1e8adc830b0b815ee46b883cc16eb250a370e89d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-01 10:57:49 +1000

    finished  the connection

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config(); // For loading env variables
+//create the connection to the database
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+
+module.exports = sequelize;
