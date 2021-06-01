323c59202f5f46382b05f70bc437a908c3ab2ef4
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-01 12:08:41 +1000

    finished the post model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//set up the post object
+class Post extends Model {}
+
+//set up the init function
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Add the reference to the user id that made it
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+//export the post object
+module.exports = Post;
