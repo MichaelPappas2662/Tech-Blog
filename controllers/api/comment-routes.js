52222e280f903c667c499ac580978ca6ca812200
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-14 12:20:36 +1000

    debugging some routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../util/auth');
 
 router.get('/', (req, res) => {
   Comment.findAll({})
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../util/auth');
 
 router.get('/', withAuth, (req, res) => {
   Post.findAll({
