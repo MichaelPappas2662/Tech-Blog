e03e1ba34f80cee0dec334b03cf9c573c922fcce
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-14 12:20:47 +1000

    debugging connections

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../util/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,48 +1,15 @@
-const { Comment } = require('../models');
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
 
-const commentData = [
-  {
-    user_id: 1,
-    post_id: 5,
-    comment_text: 'This is amazing!'
-  },
-  {
-    user_id: 4,
-    post_id: 4,
-    comment_text: 'Wow, amazing work!'
-  },
-  {
-    user_id: 1,
-    post_id: 4,
-    comment_text: 'Awesome! kudos to everyone who have contributed'
-  },
-  {
-    user_id: 3,
-    post_id: 5,
-    comment_text: 'We just reached a million subscribers! Fantastic!'
-  },
-  {
-    user_id: 3,
-    post_id: 2,
-    comment_text: 'This is great news!'
-  },
-  {
-    user_id: 3,
-    post_id: 4,
-    comment_text: 'This is one of our biggest and the most awaited feature. Keep up the good work!'
-  },
-  {
-    user_id: 5,
-    post_id: 3,
-    comment_text: 'Very useful tool!'
-  },
-  {
-    user_id: 2,
-    post_id: 1,
-    comment_text: 'Nice tool!'
-  }
-];
+const sequelize = require('../config/connection');
 
-const seedComments = () => Comment.bulkCreate(commentData);
+const seedAll = async() => {
+  await sequelize.sync({ force: true });
+  await seedUsers();
+  await seedPosts();
+  await seedComments();
+  process.exit(0);
+};
 
-module.exports = seedComments;
+seedAll();
