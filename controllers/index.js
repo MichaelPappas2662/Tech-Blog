aeb8334b6fad1c089b4046ed3947cf4463b6203d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-21 14:00:01 +1000

    changing some code

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
+
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-const apiRoutes = require('./api');
-
-router.use('/dashboard', dashboardRoutes);
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
