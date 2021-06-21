d8c6569b44325d3f5248f191660facc88c8f7561
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-21 13:59:00 +1000

    changing the code for server

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,15 @@
-// Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+//allow for use of .env data
 require('dotenv').config();
 
-// Create connection to our database, pass in your MySQL information for username and password
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// create connection to db, whether local on on heroku using jawsdb
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,42 +1,46 @@
-const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const helpers = require('./util/helpers');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+const helpers = require('./util/helpers');
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
-const session = require('express-session');
-const cool = require('cool-ascii-faces');
 
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'AlphaPup',
-  cookie: {},
-  resave: false,
+  secret: 'Super Secret Secret',
+  cookie: {
+    // Session will automatically expire in 10 minutes
+    expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
-  })
+  }),
 };
 
-const app = express()
-  .get('/cool', (req, res) => res.send(cool()));
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
-
-// turn on routes
-app.use(routes);
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(routes);
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
