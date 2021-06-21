b60a3b66467aea6eff07ed1fb14de86725ca60c5
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-21 13:59:42 +1000

    refactoring the routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,42 +1,36 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../util/auth');
 
 router.get('/', withAuth, (req, res) => {
   Post.findAll({
     where: {
+      // use the ID from the session
       user_id: req.session.user_id
     },
     attributes: [
       'id',
-      'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'post_content'
     ],
     include: [
       {
         model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'post_id',
-          'user_id',
-          'created_at'
-        ],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username']
+          attributes: ['username', 'github']
         }
       },
       {
         model: User,
-        attributes: ['username']
+        attributes: ['username', 'github']
       }
     ]
   })
     .then(dbPostData => {
+      // serialize data before passing to template
       const posts = dbPostData.map(post => post.get({ plain: true }));
       res.render('dashboard', { posts, loggedIn: true });
     })
@@ -49,44 +43,86 @@ router.get('/', withAuth, (req, res) => {
 router.get('/edit/:id', withAuth, (req, res) => {
   Post.findOne({
     where: {
-      user_id: req.session.user_id
+      id: req.params.id
     },
     attributes: [
       'id',
-      'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'post_content'
     ],
     include: [
       {
         model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'post_id',
-          'user_id',
-          'created_at'
-        ],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username']
+          attributes: ['username', 'github']
         }
       },
       {
         model: User,
-        attributes: ['username']
+        attributes: ['username', 'github']
       }
     ]
   })
     .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
       const post = dbPostData.get({ plain: true });
-      res.render('edit-post', { post, loggedIn: true });
+
+      res.render('edit-post', {
+        post,
+        loggedIn: true
+      });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
+router.get('/create/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username', 'github']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username','github']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('create-post', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
